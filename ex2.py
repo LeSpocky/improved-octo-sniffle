6eea49120be4fa13f42666990dba8fb1f6ae2cc0
Alexander Dahl <post@lespocky.de>
2016-09-29 19:38:01 +0200

    introducing ex2

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# a comment, this is so you can read your program later, or anyone else
+# anything after the # is ignored by python, also any other #
+
+print "I could have code like this." # yes and you can add comments after statements
+
+# you may also "disable" statements by adding the # in front of the line
+# print "This won't run."
+
+print "This will run."
