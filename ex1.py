1173ec9ec578452cae8372667dd89dec366a7ecb
Alexander Dahl <post@lespocky.de>
2016-09-29 13:19:17 +0200

    ex1 study drills including utf-8 text \o/

--- ex1.py
+++ ex1.py
@@ -1,7 +1,10 @@
-print "Hello World!"
-print "Hello Again"
-print "I like typing this."
-print "This is fun."
-print 'Yay! Printing.'
-print "I'd much ratger you 'not'."
-print 'I "said" do not touch this.'
+# -*- coding: utf-8 -*-
+
+#print "Hello World!"
+#print "Hello Again"
+#print "I like typing this."
+#print "This is fun."
+#print 'Yay! Printing.'
+#print "I'd much ratger you 'not'."
+#print 'I "said" do not touch this.'
+print "hübsche neue Linie haben Sie da?!"
