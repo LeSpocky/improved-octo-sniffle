5e810c01284c993ac46b561fe12e91e1d291d18d
Alexander Dahl <post@lespocky.de>
2016-09-30 07:11:15 +0200

    ex3: study drills

--- ex3.py
+++ ex3.py
@@ -1,23 +1,37 @@
+# print a line of text
 print "I will now count my chickens:"
 
+# print a string, and print a calculation
 print "Hens", 25 + 30 / 6
 print "Roosters", 100 - 25 * 3 % 4
 
+# print a line of text
 print "Now I will count the eggs:"
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# print just a calculation
+print 3 + 2 + 1 - 5 + 4 % 2 - 1.0 / 4 + 6
 
+# print a line of text
 print "Is it true that 3 + 2 < 5 -7?"
 
+# print a just calculation with a compare operator
 print 3 + 2 < 5 - 7
 
+# print a string, and print a calculation
 print "What is 3 + 2?", 3 + 2
 print "What is 5 - 7?", 5 - 7
 
+# print a line of text
 print "Oh, that's why it's False."
 
+# print a line of text
 print "How about some more."
 
+# print some strings, then print comparisions
 print "Is it greater?", 5 > -2
 print "Is it greater or equal?", 5 >= -2
 print "Is it less or equal?", 5 <= 2
+
+# the difference of integer to float arithmetic
+print 7 / 4
+print 7.0 / 4.0
