09b62786fa8ab2c04bcdbd067eb509c6826ac39c
Alexander Dahl <post@lespocky.de>
2017-01-20 08:36:21 +0100

    ex33: study drills 1 and 2

--- ex33.py
+++ ex33.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-i = 0
-numbers = []
+def my_loop( limit ):
+    i = 0
+    numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append( i )
+    while i < limit:
+        print "At the top i is %d" % i
+        numbers.append( i )
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+        i = i + 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
+
+    return numbers
 
 
 print "The numbers: "
 
-for num in numbers:
+for num in my_loop( 6 ):
     print num
